fbce5c3ea2f91718fe885ba7e0eb107f188cd4b6
Jyoti Avhad <avhadjyoti09@gmail.com>
2019-12-20 11:24:23 +0530

    Display Text using Kotlin file

--- My First Demo/app/src/main/java/com/example/myfirstdemo/MainActivity.kt
+++ My First Demo/app/src/main/java/com/example/myfirstdemo/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.myfirstdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
+// Class declarations and extending AppCompatActivity
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    // Overriding class methods, No need of @ annotation.
+    override fun onCreate(savedInstanceState: Bundle?) //savedInstanceState can be null or Bundle type.
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //local variable declaration (can be either val or var)
+        val helloKotlin = "Hello Kotlin...!"
+        displayHelloKotlin(helloKotlin)
+
+    }
+
+    // Private function declaration
+    private fun displayHelloKotlin(helloKotlin: String) {
+
+        // We can directly call the id of the textView
+        tvHelloKotlin.text = helloKotlin
     }
 }
