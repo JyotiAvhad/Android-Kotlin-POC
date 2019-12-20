a7bedff41e2f826f988cd718f8266ac93d88bfc9
Jyoti Avhad <avhadjyoti09@gmail.com>
2019-12-20 18:43:24 +0530

    Modifying Files

--- OptionMenuDemo/app/src/main/java/com/example/optionmenudemo/MainActivity.kt
+++ OptionMenuDemo/app/src/main/java/com/example/optionmenudemo/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.LinearLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,4 +22,15 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item!!.itemId){
+            R.id.firstMenu -> Toast.makeText(this,"You Select First Menu",Toast.LENGTH_SHORT).show()
+            R.id.secondMenu -> Toast.makeText(this,"You Select Second Menu",Toast.LENGTH_SHORT).show()
+            R.id.thirdMenu -> Toast.makeText(this,"You Select Third Menu",Toast.LENGTH_SHORT).show()
+            R.id.fourthMenu -> Toast.makeText(this,"You Select Fourth Menu",Toast.LENGTH_SHORT).show()
+            R.id.fifthMenu -> Toast.makeText(this,"You Select Fifth Menu",Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
