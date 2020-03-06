3e04d02a2be126e6884f1d144e232735e3c78d71
Rishikesh Gaikar <rishikeshgaikar07@gmail.com>
2020-03-06 16:59:59 +0530

    Set Text Scrollable

--- My First Demo/app/src/main/java/com/example/myfirstdemo/MainActivity.kt
+++ My First Demo/app/src/main/java/com/example/myfirstdemo/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.example.myfirstdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 // Class declarations and extending AppCompatActivity
@@ -14,8 +15,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //local variable declaration (can be either val or var)
-        val helloKotlin = "Hello Kotlin...!"
+        val helloKotlin = "Kotlin"
         displayHelloKotlin(helloKotlin)
+
+        tv_kotlin_text.text =
+            "Kotlin is a cross-platform, statically typed, general-purpose programming language with type inference. " +
+                    "Kotlin is designed to inter-operate fully with Java, and the JVM version of its standard library depends on the Java Class Library, " +
+                    "but type inference allows its syntax to be more concise."
+
+        //to set text in a single line
+        tv_kotlin_text.setSingleLine()
+
+        //to set text scrollable or flowable automatically in a single line
+        tv_kotlin_text.ellipsize = TextUtils.TruncateAt.MARQUEE
+        //-1 so scrolling will happen infinite times. If we give 1, then scrolling will happen only once.
+        tv_kotlin_text.marqueeRepeatLimit = -1
+        tv_kotlin_text.isSelected = true
+
 
     }
 
@@ -23,6 +39,7 @@ class MainActivity : AppCompatActivity() {
     private fun displayHelloKotlin(helloKotlin: String) {
 
         // We can directly call the id of the textView
-        tvHelloKotlin.text = helloKotlin
+        tv_HelloKotlin.text = helloKotlin
     }
+
 }
