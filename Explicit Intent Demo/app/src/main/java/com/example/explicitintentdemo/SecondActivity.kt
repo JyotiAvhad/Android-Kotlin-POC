54da36b24017b737b017684166fb8b40cb2ec63d
JyotiAvhad <avhadjyoti09@gmail.com>
2020-06-18 15:42:55 +0530

    Implement Demo using Kotlin

--- Explicit Intent Demo/app/src/main/java/com/example/explicitintentdemo/SecondActivity.kt
+++ Explicit Intent Demo/app/src/main/java/com/example/explicitintentdemo/SecondActivity.kt
@@ -4,18 +4,24 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_second.*
 
-class MainActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn_clickMe.setOnClickListener() {
-            intent = Intent(this, SecondActivity::class.java)
-            intent.putExtra("id_value", id)
-            intent.putExtra("language_value", language)
+        val bundle: Bundle? = intent.extras
+        val id = bundle?.get("id_value")
+        val language = bundle?.get("language_value")
+
+        Toast.makeText(applicationContext, id.toString() + " " + language, Toast.LENGTH_LONG).show()
+        btn_clickSecond.setOnClickListener() {
+
+            intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
