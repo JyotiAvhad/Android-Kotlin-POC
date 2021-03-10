757973083b5cbe5d913c06fa85fdd3e7f588d947
JyotiAvhad <avhadjyoti09@gmail.com>
2021-03-10 11:30:38 +0530

    remove edititext underline

--- Text Input Layout/app/src/main/java/com/example/textinputlayout/MainActivity.kt
+++ Text Input Layout/app/src/main/java/com/example/textinputlayout/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.textinputlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun welcome(view: View) {
+
+        Toast.makeText(this,"Sign in successful ....!",Toast.LENGTH_SHORT).show()
     }
 }
